92789cfca492be63753d5c1a15f75226f1826fe6
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-03-15 17:46:10 -0400

    ENH: Adding licence and cleaner syntax for object instatantiation

--- examples/test_tsd_syn.py
+++ examples/test_tsd_syn.py
@@ -1,16 +1,25 @@
-# [THINSHELLDEMONS] Read Mesh and Create TSD object
-
-# Build the Module with Python Wrapping and import the itk package which is available under Wrapping/Generators/Python
-#import sys
-#sys.path.append('/home/pranjal.sahu/ITKPR7/ITK/ITK-build/Wrapping/Generators/Python')
-
-import itkConfig
-itkConfig.LazyLoading = False
+"""
+=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================
+ """
 
 import numpy as np
 import itk
-from itk import itkThinShellDemonsMetricv4Python as tsd
-from itk import itkConjugateGradientLineSearchOptimizerv4Python as itkc
 import math
 
 
@@ -110,7 +119,7 @@ print('Transform Created')
 print(transform)
 
 
-MetricType = tsd.itkThinShellDemonsMetricv4MD3
+MetricType = itk.ThinShellDemonsMetricv4[type(fixedMesh)]
 metric = MetricType.New()
 metric.SetStretchWeight(1)
 metric.SetBendWeight(5)
